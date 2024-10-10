74a9e6cb29492ec503c95cd18fa95f674b22b4e2
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-10-10 06:44:10 +0200

    ajout du tri par date - order pub date

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,6 +7,7 @@ from .models import Choice
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date"]
     list_filter = ["question_text", "pub_date"]
+    ordering = ["pub_date"]
 
 
 admin.site.register(Question, QuestionAdmin)
